952b20b847bf8bce716505263a9cb626e940ac2b
DeadFred <f.cliff1@hotmail.com>
2018-01-17 14:06:00 +1000

    Created basic landing page with hidden text input for Procedures, added React-Router-DOM and created blank Inventory page.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,46 @@
 import React, { Component } from 'react';
 import './App.css';
+import { Title, Field, Control, Input, Button } from 'reactbulma'
+
 
 class App extends Component {
+
+state = {
+  // procedure search enable/disable
+  procSelect: false
+}
+
+showSearch = () => {
+  console.log('State Changed!')
+  this.setState(prevState => ({
+    procSelect: !prevState.procSelect
+  }))
+}
+
   render() {
+    
+    const { procSelect } = this.state
+
     return (
       <div className="App">
-      
+        <div className="ProcInvSelector">
+          <Title onClick={this.showSearch}>Procedures</Title>
+          {
+            procSelect &&
+            <Field hasAddons>
+              <Control>
+                <Input placeholder="Enter the name of a procedure:" />
+              </Control>
+              <Control>
+                <Button info>
+                  Search
+                </Button>
+              </Control>
+            </Field>
+          }
+          <hr/>
+          <Title>Inventory</Title>
+        </div>
       </div>
     );
   }
