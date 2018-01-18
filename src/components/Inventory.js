a59d710066fd898f0288a1334ef50853781640f4
mail@christophermaier.com.au <mail@christophermaier.com.au>
2018-01-18 11:57:08 +1000

    updated inventory to show table

--- src/components/Inventory.js
+++ src/components/Inventory.js
@@ -1,13 +1,42 @@
-import React, { Component } from 'react';
-import { Title } from 'reactbulma';
+import React, {Component} from 'react';
+import TableHeader from 'grommet/components/TableHeader';
+import App from 'grommet/components/App';
+import TableRow from 'grommet/components/TableRow';
+import Table from 'grommet/components/Table';
 
 class Inventory extends Component {
   render() {
-    return (
-      <div className="invTable">
-        <Title>Test Heading</Title>
-      </div>
-    )
+    return (<App>
+      <Table>
+        <TableHeader labels={['Name', 'Note']} sortIndex={0} sortAscending={true}/>
+        <tbody>
+          <TableRow>
+            <td>
+              Alan
+            </td>
+            <td>
+              plays accordion
+            </td>
+          </TableRow>
+          <TableRow>
+            <td>
+              Chris
+            </td>
+            <td>
+              drops the mic
+            </td>
+          </TableRow>
+          <TableRow>
+            <td>
+              Tracy
+            </td>
+            <td>
+              travels the world
+            </td>
+          </TableRow>
+        </tbody>
+      </Table>
+    </App>)
   }
 }
 
