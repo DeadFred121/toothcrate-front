5c25a23eca7e072402204aff9e4cbb5480f087b4
mail@christophermaier.com.au <mail@christophermaier.com.au>
2018-01-18 14:04:14 +1000

    working on footer

--- src/App.js
+++ src/App.js
@@ -15,12 +15,6 @@ import Inventory from './components/Inventory'
 import ModeSelect from './components/ModeSelect'
 import NavBar from './components/NavBar'
 import Toothfooter from './components/ToothFooter'
-import Box from 'grommet/components/Box'
-import Paragraph from 'grommet/components/Paragraph'
-import Menu from 'grommet/components/Menu'
-import Anchor from 'grommet/components/Anchor'
-import Footer from 'grommet/components/Footer'
-import logo from './images/TCLogo.png'
 import '../node_modules/grommet/grommet-hpinc.min.css'
 
 class App extends Component {
@@ -36,40 +30,11 @@ state = {
       <Router>
         <div className="App" centered='true' inline={true}>
           <NavBar />
-          <div className="content">
           <Switch>
             <Route exact path="/" component={ ModeSelect }/>
             <Route path="/inventory" component={ Inventory }/>
           </Switch>
-        </div>
-          <Footer justify='between'>
-            <Title>
-              <s/>
-              <a href="/"><Image src={logo} size='thumb'/>
-              </a>
-            </Title>
-            <Box direction='row' align='center' pad={{
-                "between" : "medium"
-              }}>
-              <Paragraph margin='none'>
-                © 2017-2018 Invent/Story
-              </Paragraph>
-              <Menu direction='row' size='small' dropAlign={{
-                  "right" : "right"
-                }}>
-                <Anchor href='#'>
-                  Support
-                </Anchor>
-                <Anchor href='#'>
-                  Contact
-                </Anchor>
-                <Anchor href='#'>
-                  About
-                </Anchor>
-              </Menu>
-            </Box>
-          </Footer>
-          {/* <Toothfooter /> */}
+          <Toothfooter />
         </div>
       </Router>
     );
